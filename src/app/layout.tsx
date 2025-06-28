fea776153a087c567056edd6c0e555a7d0059722
Aahad Vakani <aahadvakani@Aahads-MacBook-Pro-2286.local>
2025-06-28 07:08:46 -0400

    PostHog Integration test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import { Toaster } from "@/components/ui/toaster"
-import { AnalyticsWrapper } from './providers'
+import { PostHogProvider } from './providers'
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -87,9 +87,10 @@ export default function RootLayout({
         />
       </head>
       <body className={inter.className}>
+      <PostHogProvider>
         {children}
         <Toaster />
-        <AnalyticsWrapper />
+      </PostHogProvider>
       </body>
     </html>
   )
